3859ec4dd96fe3bcea7beb0c31e7b23369fc4027
2021051321 <tatigalle23@gmail.com>
2023-12-26 21:27:40 +0900

    main and script

---
+++ Views/script.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("addBookForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("addBookForm").style.display = "none";
+  }
